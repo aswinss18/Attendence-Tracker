e9ead10144d4920db521c25d4f2064cd7c8376b4
Aswin <aswinss0018@gmail.com>
2025-04-29 21:56:56 +0530

    user selector  component created

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -1,10 +1,12 @@
 import Loader from "@/components/common/Loader";
+import UserSelector from "@/components/layout/UserSelector";
 import React from "react";
 
 export default function page() {
   return (
     <div>
-      <Loader />
+      {/* <Loader /> */}
+      <UserSelector />
     </div>
   );
 }
