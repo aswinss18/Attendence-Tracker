15f20a395b38d3cdaee019686fc9819bb03ca30e
Aswin <aswinss0018@gmail.com>
2025-04-29 22:31:08 +0530

    bug fix

--- src/app/admin/users/page.js
+++ src/app/admin/users/page.js
@@ -1,5 +1,7 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { useRouter } from "next/router";
 
 const AddUser = () => {
   const [formData, setFormData] = useState({
